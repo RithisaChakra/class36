8fb21a541c1d6bba85d4e14d47eec65de64f20a9
RithisaChakra <76734385+RithisaChakra@users.noreply.github.com>
2021-04-15 17:04:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  background("white");
+  
+    drawSprites();
+  
+}
